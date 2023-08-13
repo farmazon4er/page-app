54753f00f1bd68ab61e39fd3298ef32f205c6c04
Vasilii Bulkosvkii <vasilii.bulkovskii@gmail.com>
2023-08-13 16:49:41 +0500

    add ancestry

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,5 @@
 class Page < ApplicationRecord
-  has_many :children, class_name: "Pages", foreign_key: "pages_id"
-  belongs_to :parent, class_name: "Pages"
-
+  has_ancestry
+  
   validates :name, presence: true , format: { with: /[a-zA-Z0-9_]/ }
 end
